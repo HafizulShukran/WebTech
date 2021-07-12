7010a61e95f9783ff1e3c73fb213cb170926b0a4
Hafizul <75723090+HafizulShukran@users.noreply.github.com>
2021-07-13 00:37:59 +0800

    base ui
    mostly view data.
    One user

---
+++ api/db.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
+
+    class db{
+        private $host = 'localhost';
+        private $user = 'root';
+        private $password = '';
+        private $dbname = 'hoteldb';
+
+        public function connect(){
+            $mysql_connect_str = "mysql:host=$this->host;dbname=$this->dbname";
+            $dbConnection = new PDO($mysql_connect_str, $this->user, $this->password);
+            $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $dbConnection;
+        }
+    }
---
+++ api/getAllRoom.php
@@ -0,0 +1,22 @@
+<?php
+
+header('Content-Type: application/json');
+
+require 'db.php';
+
+$sql = "SELECT * from room";
+
+try {
+    $db = new db();
+    $db = $db->connect();
+
+    $stmt = $db->query($sql);
+    $user = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    echo json_encode($user);
+} catch (PDOException $e) {
+    $data = array(
+        "status" => "fail"
+    );
+    echo json_encode($data);
+}
