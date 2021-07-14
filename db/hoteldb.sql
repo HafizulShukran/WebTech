35ef83f041247b94a6a1aeee9b6701740d89a303
Hafizul <75723090+HafizulShukran@users.noreply.github.com>
2021-07-14 22:58:13 +0800

    update db

--- db/hoteldb.sql
+++ db/hoteldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2021 at 09:44 AM
+-- Generation Time: Jul 14, 2021 at 04:57 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -20,6 +20,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `hoteldb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `bookingID` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `roomNo` varchar(255) NOT NULL,
+  `roomType` varchar(255) NOT NULL,
+  `roomPrice` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `roomID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -45,9 +61,42 @@ INSERT INTO `room` (`roomID`, `roomNo`, `roomType`, `roomImage`, `roomPrice`, `r
 (2, '136', 'single', 0x64656661756c74526f6f6d2e706e67, 20, 'available'),
 (3, '137', 'single', 0x64656661756c74526f6f6d2e706e67, 20, 'available');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `phoneNo` int(11) DEFAULT NULL,
+  `picture` varchar(255) NOT NULL DEFAULT 'default.png'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `username`, `password`, `name`, `email`, `role`, `phoneNo`, `picture`) VALUES
+(1, 'JohnDoe', 123, 'John Doe', 'test@email.com', 'admin', 123456789, 'default.png'),
+(2, 'JaneDoe', 123, 'Jane Doe', 'test2@email.com', 'user', 1222222, 'default.png');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`bookingID`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `roomID` (`roomID`);
 
 --
 -- Indexes for table `room`
@@ -56,14 +105,43 @@ ALTER TABLE `room`
   ADD PRIMARY KEY (`roomID`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `bookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
   MODIFY `roomID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`roomID`) REFERENCES `room` (`roomID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
