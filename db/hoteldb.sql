7eea6cba8422faf144016e37ccc403b73eae17e4
Burhanuddin99 <muhdburhanuddin12@gmail.com>
2021-07-20 11:53:50 +0800

    new hoteldb.sq

--- db/hoteldb.sql
+++ db/hoteldb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2021 at 04:57 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
+-- Generation Time: Jul 20, 2021 at 05:52 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,22 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `hoteldb`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `booking`
---
-
-CREATE TABLE `booking` (
-  `bookingID` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `roomNo` varchar(255) NOT NULL,
-  `roomType` varchar(255) NOT NULL,
-  `roomPrice` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `roomID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -59,44 +43,13 @@ CREATE TABLE `room` (
 INSERT INTO `room` (`roomID`, `roomNo`, `roomType`, `roomImage`, `roomPrice`, `roomStatus`) VALUES
 (1, '123', 'single', 0x64656661756c74526f6f6d2e706e67, 20, 'available'),
 (2, '136', 'single', 0x64656661756c74526f6f6d2e706e67, 20, 'available'),
-(3, '137', 'single', 0x64656661756c74526f6f6d2e706e67, 20, 'available');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `userID` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  `phoneNo` int(11) DEFAULT NULL,
-  `picture` varchar(255) NOT NULL DEFAULT 'default.png'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`userID`, `username`, `password`, `name`, `email`, `role`, `phoneNo`, `picture`) VALUES
-(1, 'JohnDoe', 123, 'John Doe', 'test@email.com', 'admin', 123456789, 'default.png'),
-(2, 'JaneDoe', 123, 'Jane Doe', 'test2@email.com', 'user', 1222222, 'default.png');
+(3, '137', 'single', 0x64656661756c74526f6f6d2e706e67, 20, 'available'),
+(4, '205', 'Double', 0x64656661756c74526f6f6d2e6a7067, 34, 'unavailable'),
+(5, '146', 'Quad', 0x64656661756c74526f6f6d2e6a7067, 20, 'available');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `booking`
---
-ALTER TABLE `booking`
-  ADD PRIMARY KEY (`bookingID`),
-  ADD KEY `userID` (`userID`),
-  ADD KEY `roomID` (`roomID`);
 
 --
 -- Indexes for table `room`
@@ -105,43 +58,14 @@ ALTER TABLE `room`
   ADD PRIMARY KEY (`roomID`);
 
 --
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `booking`
---
-ALTER TABLE `booking`
-  MODIFY `bookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `roomID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `booking`
---
-ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`),
-  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`roomID`) REFERENCES `room` (`roomID`);
+  MODIFY `roomID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
